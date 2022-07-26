a95c5e0900fc509015af73227dd761d38ef4c4f2
Laura Vedovatto <laura.vedovatto@gmail.com>
2022-07-25 22:20:55 -0300

    Service integration

---
+++ keyboard/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  keyboard
+//
+//  Created by Felipe Melo on 25/07/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let baseURL = "https://frontend-coding-challenge-api.herokuapp.com/getContent"
+}
